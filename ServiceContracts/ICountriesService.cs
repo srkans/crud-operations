6f861565e544c7db9fa01cbe2f1c1d2c1c9809a2
srkans <serkansacma@hotmail.com>
2023-04-17 09:45:02 +0300

    dummy CountriesService has been created for xUnit test

--- ServiceContracts/ICountriesService.cs
+++ ServiceContracts/ICountriesService.cs
@@ -7,6 +7,11 @@ namespace ServiceContracts
     /// </summary>
     public interface ICountriesService
     {
+        /// <summary>
+        /// Adds a country object to the list of countries
+        /// </summary>
+        /// <param name="countryAddRequest">Country object to add</param>
+        /// <returns>Returns the country object after adding it(including newly generated country id)</returns>
        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
     }
 }
