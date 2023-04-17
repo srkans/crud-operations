749082d4c8590334813ea1d4bd6b5c11e3f09923
srkans <serkansacma@hotmail.com>
2023-04-17 12:26:17 +0300

    passed ProperCountryName test 1 of 4

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,13 +1,30 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            //Convert object from CountryAddRequest to Country type--from DTO to DomainModel
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into _countries
+            _countries.Add(country);
+
+            return country.ToCountryResponse(); //bu methoddaki yapı sayesinde domain model'i servis'in dışına göstermemiş oluyoruz.
         }
     }
 }
