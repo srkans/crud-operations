c8d40e2fbae5ead3ebdbd44f1927125fd57272b8
srkans <serkansacma@hotmail.com>
2023-04-17 10:51:51 +0300

    necessary tests has been written first TDD

--- ServiceContracts/DTO/CountryResponse.cs
+++ ServiceContracts/DTO/CountryResponse.cs
@@ -11,6 +11,13 @@ namespace ServiceContracts.DTO
         public Guid CountryID { get; set; }
         public string? CountryName { get; set; }
 
-        public Country ToCountry() { return new Country {  CountryID = CountryID, CountryName = CountryName }; }
+    }
+
+    public static class CountryExtensions
+    {
+        public static CountryResponse ToCountryResponse(this Country country) 
+        {
+            return new CountryResponse() { CountryID = country.CountryID, CountryName = country.CountryName };
+        }
     }
 }
