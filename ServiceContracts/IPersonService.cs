5dd21cf9f6209121b4e3cab71fcd65058216ab30
srkans <serkansacma@hotmail.com>
2023-04-19 11:17:08 +0300

    IPersonService, service contract has been created

---
+++ ServiceContracts/IPersonService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    /// <summary>
+    /// Represents business logic for manipulating Person Entity
+    /// </summary>
+    public interface IPersonService
+    {
+        /// <summary>
+        /// Adds a new person into the list of Persons
+        /// </summary>
+        /// <param name="personAddRequest">Person to add</param>
+        /// <returns>Returns the same person details, along with newly generated PersonID</returns>
+        PersonResponse AddPerson(PersonAddRequest personAddRequest);
+
+        /// <summary>
+        /// Returns all persons
+        /// </summary>
+        /// <returns>Returns a list of objects of PersonResponse type</returns>
+        List<PersonResponse> GetAllPersons();
+    }
+}
