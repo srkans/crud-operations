5d90385eb236f8841ce13125e9762329e4289b43
srkans <serkansacma@hotmail.com>
2023-05-10 10:41:51 +0300

    CountriesRepository

---
+++ Repositories/CountriesRepository.cs
@@ -0,0 +1,40 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+
+namespace Repositories
+{
+    public class CountriesRepository : ICountriesRepository
+    {
+
+        private readonly ApplicationDbContext _dbContext;
+
+        public CountriesRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Country> AddCountry(Country country)
+        {
+            _dbContext.Countries.Add(country);
+            await _dbContext.SaveChangesAsync();
+
+            return country;
+        }
+
+        public async Task<List<Country>> GetAllCountries()
+        {
+            return await _dbContext.Countries.ToListAsync();
+        }
+
+        public async Task<Country?> GetCountryByCountryName(string countryName)
+        {
+            return await _dbContext.Countries.FirstOrDefaultAsync(temp => temp.CountryName == countryName);
+        }
+
+        public async Task<Country?> GetCountryById(Guid countryID)
+        {
+            return await _dbContext.Countries.FirstOrDefaultAsync(temp => temp.CountryID == countryID);
+        }
+    }
+}
