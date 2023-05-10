2a3b1ec5786e09333c43ae185397d433785d9281
srkans <serkansacma@hotmail.com>
2023-05-10 11:04:16 +0300

    PersonsRepository

---
+++ Repositories/PersonsRepository.cs
@@ -0,0 +1,75 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class PersonsRepository : IPersonsRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public PersonsRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Person> AddPerson(Person person)
+        {
+            _dbContext.Persons.Add(person);
+            await _dbContext.SaveChangesAsync();
+
+            return person;
+        }
+
+        public async Task<bool> DeletePersonByPersonID(Guid personID)
+        {
+            _dbContext.Persons.RemoveRange(_dbContext.Persons.Where(temp => temp.PersonID == personID));
+            int rowsDeleted = await _dbContext.SaveChangesAsync();
+
+            return rowsDeleted>0;
+        }
+
+        public async Task<List<Person>> GetAllPersons()
+        {
+            return await _dbContext.Persons.Include("Country").ToListAsync();
+        }
+
+        public async Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate)
+        {
+            return await _dbContext.Persons.Include("Country").Where(predicate).ToListAsync();
+        }
+
+        public async Task<Person?> GetPersonByPersonID(Guid personID)
+        {
+            return await _dbContext.Persons.Include("Country").FirstOrDefaultAsync(temp => temp.PersonID == personID);
+        }
+
+        public async Task<Person> UpdatePerson(Person person)
+        {
+            Person? matchingPerson = await _dbContext.Persons.FirstOrDefaultAsync(temp => temp.PersonID == person.PersonID);
+
+            if (matchingPerson == null)
+            {
+                return person;
+            }
+
+            matchingPerson.Name = person.Name;
+            matchingPerson.Email = person.Email;
+            matchingPerson.Gender = person.Email;
+            matchingPerson.DateOfBirth = person.DateOfBirth;
+            matchingPerson.CountryID = person.CountryID;
+            matchingPerson.Address = person.Address;
+            matchingPerson.ReceiveNewsLetters = person.ReceiveNewsLetters;
+
+            await _dbContext.SaveChangesAsync();
+
+            return matchingPerson;
+        }
+    }
+}
