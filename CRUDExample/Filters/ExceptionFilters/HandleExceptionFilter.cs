7a99135b6cabf479e77494f20e235d7fb5428888
srkans <serkansacma@hotmail.com>
2023-05-16 17:06:31 +0300

    Exception Filter

---
+++ CRUDExample/Filters/ExceptionFilters/HandleExceptionFilter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ExceptionFilters
+{
+    public class HandleExceptionFilter : IAsyncExceptionFilter
+    {
+        private readonly ILogger<HandleExceptionFilter> _logger;
+        private readonly IHostEnvironment _environment;
+
+        public HandleExceptionFilter(ILogger<HandleExceptionFilter> logger, IHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+
+        }
+        public async Task OnExceptionAsync(ExceptionContext context)
+        {
+            _logger.LogError("Exception filter {FilterName}.{MethodName}\n{ExceptionType}\n{ExceptionMessage}",nameof(HandleExceptionFilter),nameof(OnExceptionAsync),context.Exception.GetType().ToString(),context.Exception.Message);
+
+            if (_environment.IsDevelopment())
+            {
+                context.Result = new ContentResult() { Content = context.Exception.Message, StatusCode = 500 };
+            }
+
+        }
+    }
+}
