09bbedf1122d36ebb0ce5bae2c3282b5c843c21b
srkans <serkansacma@hotmail.com>
2023-05-23 14:41:16 +0300

    custom exceptions

---
+++ Exceptions/InvalidPersonIDException.cs
@@ -0,0 +1,20 @@
+﻿namespace Exceptions
+{
+    public class InvalidPersonIDException : ArgumentException
+    {
+        public InvalidPersonIDException() 
+        { 
+        }
+
+        public InvalidPersonIDException(string? message) : base(message)
+        {
+        }
+
+        public InvalidPersonIDException(string? message, Exception? innerException) : base(message,innerException)
+        {
+
+        }
+
+
+    }
+}
